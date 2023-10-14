0d442fcb5ef650c7eba7e121219c421b17bd0c47
ichouare <issam.chouare1998@gmail.com>
2023-10-14 18:31:45 +0100

    here

--- requirements/mariadb/conf/db.sh
+++ requirements/mariadb/conf/db.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-service  mariadb start  && mysql -u root <<"./core.sql"
+service mariadb start;
+mysql -u root <  "./core.sql"
+sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+service mariadb restart
+mysql
--- requirements/wordpress/tools/script.sh
+++ requirements/wordpress/tools/script.sh
@@ -12,9 +12,9 @@ sed -i -r "s/define\( 'DB_NAME', '.*' \);/define( 'DB_NAME', 'WORDPRESSDB' );/" 
 sed -i -r "s/define\( 'DB_USER', '.*' \);/define( 'DB_USER', 'ichouare' );/" /var/www/wordpress/wp-config.php
 sed -i -r "s/define\( 'DB_PASSWORD', '.*' \);/define( 'DB_PASSWORD', '1998' );/" /var/www/wordpress/wp-config.php
 sed -i -r "s/define\( 'HOST', '.*' \);/define( 'HOST', 'mariadb:3306' );/" /var/www/wordpress/wp-config.php
-chown -R www-data:www-data /var/www/wordpress/wp-config.php
-sleep 10
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0: 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-service php7.4-fpm start
+#chown -R www-data:www-data /var/www/wordpress/wp-config.php
+#sleep 10
+#sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+#service php7.4-fpm start
 echo "creat run/php floder"
 echo "star wordpress" && /usr/sbin/php-fpm7.4 -F
