339e222748fdab021d307d004d370e5ed2abeea0
ichouare <issam.chouare1998@gmail.com>
2023-10-15 17:34:00 +0100

    here

--- requirements/mariadb/conf/db.sh
+++ requirements/mariadb/conf/db.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-service mariadb start;
+
+service mariadb start
+echo "hello from mariadb"
+sleep 10
+#service mariadb start
 mysql -u root <  "./core.sql"
 sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-service mariadb restart
-mysql
+echo "mariadb start"
+mysqladmin -u root   shutdown
+mysqld
+
--- requirements/wordpress/tools/script.sh
+++ requirements/wordpress/tools/script.sh
@@ -8,10 +8,10 @@ mv wp-cli.phar /usr/local/bin/wp
 
 mv /wp-config.php /var/www/wordpress/wp-config.php
 
-sed -i -r "s/define\( 'DB_NAME', '.*' \);/define( 'DB_NAME', 'WORDPRESSDB' );/" /var/www/wordpress/wp-config.php
+sed -i -r "s/define\( 'DB_NAME', '.*' \);/define( 'DB_NAME', 'wp_db' );/" /var/www/wordpress/wp-config.php
 sed -i -r "s/define\( 'DB_USER', '.*' \);/define( 'DB_USER', 'ichouare' );/" /var/www/wordpress/wp-config.php
 sed -i -r "s/define\( 'DB_PASSWORD', '.*' \);/define( 'DB_PASSWORD', '1998' );/" /var/www/wordpress/wp-config.php
-sed -i -r "s/define\( 'HOST', '.*' \);/define( 'HOST', 'mariadb:3306' );/" /var/www/wordpress/wp-config.php
+sed -i -r "s/define\( 'HOST', '.*' \);/define( 'HOST', 'mariadb' );/" /var/www/wordpress/wp-config.php
 #chown -R www-data:www-data /var/www/wordpress/wp-config.php
 #sleep 10
 #sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
