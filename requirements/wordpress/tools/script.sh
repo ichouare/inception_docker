138a096185764a033d7b4ca5275b18ba7ede9813
ichouare <issam.chouare1998@gmail.com>
2023-10-16 18:27:28 +0100

    here

--- requirements/wordpress/tools/script.sh
+++ requirements/wordpress/tools/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 15
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 sleep 10
 chmod +x wp-cli.phar
@@ -12,9 +13,8 @@ sed -i -r "s/define\( 'DB_NAME', '.*' \);/define( 'DB_NAME', 'wp_db' );/" /var/w
 sed -i -r "s/define\( 'DB_USER', '.*' \);/define( 'DB_USER', 'ichouare' );/" /var/www/wordpress/wp-config.php
 sed -i -r "s/define\( 'DB_PASSWORD', '.*' \);/define( 'DB_PASSWORD', '1998' );/" /var/www/wordpress/wp-config.php
 sed -i -r "s/define\( 'HOST', '.*' \);/define( 'HOST', 'mariadb' );/" /var/www/wordpress/wp-config.php
-#chown -R www-data:www-data /var/www/wordpress/wp-config.php
-#sleep 10
-#sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-#service php7.4-fpm start
-echo "creat run/php floder"
+
+/usr/local/bin/wp core install --url=ichouare.42.fr  --title=inception --admin_user=ichouare --admin_password=1998 --admin_email=issam.chouaref1998@gmail.com --skip-email=  --path='/var/www/wordpress' --allow-root
+
+echo "Base wordpress  config completed"
 echo "star wordpress" && /usr/sbin/php-fpm7.4 -F
