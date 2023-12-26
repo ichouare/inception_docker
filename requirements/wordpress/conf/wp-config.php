bdd3882f0d7c4a5528e3dd0543c42d39744115e8
ichouare <issam.chouare1998@gmail.com>
2023-12-26 17:33:20 +0100

    update

--- requirements/wordpress/conf/wp-config.php
+++ requirements/wordpress/conf/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'WORDPRESSDB' );
+define( 'DB_NAME', '$db_name' );
 
 /** Database username */
-define( 'DB_USER', 'ichouare' );
+define( 'DB_USER', '$db_user' );
 
 /** Database password */
-define( 'DB_PASSWORD', '1998' );
+define( 'DB_PASSWORD', '$db_pwd' );
 
 /** Database hostname */
 define( 'DB_HOST', 'mariadb' );
