7f50c6f1ba2bffd0ad85112f96af1dc3634ec830
ichouare <issam.chouare1998@gmail.com>
2024-01-08 12:45:39 +0100

    inception 42

--- srcs/requirements/mariadb/conf/db.sh
+++ srcs/requirements/mariadb/conf/db.sh
@@ -5,13 +5,15 @@ echo "hello from mariadb"
 sleep 10
 echo "CREATE DATABASE IF NOT EXISTS  ${db_name};" > ./core.sql
 echo "CREATE USER '${db_user}'@'%' IDENTIFIED BY '${db_pwd}';" >> ./core.sql
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '${db_pwd}'; " >> ./core.sql
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '${db_pwd}; " >> ./core.sql
 echo "CREATE USER '${db1_user}'@'%' IDENTIFIED BY '${db1_pwd}';" >> ./core.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';" >> ./core.sql
+echo "GRANT ALL PRIVILEGES ON ${db_name}.* TO '${db_user}'@'%';" >> ./core.sql
 echo "FLUSH PRIVILEGES" >> ./core.sql
-mysql -u root <  "./core.sql"
+mysql  <  "./core.sql"
+# replace occurrences of 127.0.0.1 with 0.0.0.0 in the file 
 sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 echo "mariadb start"
 mysqladmin -u root   shutdown
+#start the Mysql server daemon
 mysqld
 
